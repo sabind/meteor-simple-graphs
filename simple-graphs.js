cae4e47d7f8aace38ef8378c882923bd59af7e94
Dan Sabin <dan.sabin@gmail.com>
2015-01-29 18:30:20 -0800

    working graphs

--- simple-graphs.js
+++ simple-graphs.js
@@ -9,6 +9,18 @@ var keyValueArray = function(obj) {
   return array;
 }
 
+var keyValueArrayCustomKeyValueName = function(obj, keyName, valueName) {
+  var array = [];
+  for (var prop in obj) {
+    if (obj.hasOwnProperty(prop))
+      element = {}
+      element[keyName] = prop;
+      element[valueName] = obj[prop];
+      array.push(element)
+  }
+  return array;
+}
+
 if (Meteor.isClient) {
 
   Template.table_of_data.helpers({
@@ -66,6 +78,86 @@ if (Meteor.isClient) {
       return false;
     }
   });
+  
+  Template.graph_by_category.rendered = function() {
+    var w = 600;
+    var h = 250;
+    
+    var xScale = d3.scale.ordinal().rangeRoundBands([0, w], 0.05);
+    var yScale = d3.scale.linear().range([0, h]);
+    
+    var svg = d3.select("#chart").attr("width", w).attr("height", h);
+    
+    var key = function(doc) {
+      return doc.category;
+    }
+    Deps.autorun(function() {
+      var allData = DataEvents.find({}).fetch();
+     
+      var dataset = [];
+      var sums = {};
+
+      _.map(allData, function (eventPoint) {
+        var aggregate = sums[eventPoint.category] || 0;
+        aggregate += eventPoint.amount;
+        sums[eventPoint.category] = aggregate;
+      });
+      
+      console.log('allData', allData);
+      console.log('sums', sums);
+      
+      dataset = keyValueArrayCustomKeyValueName(sums, 'category', 'amount');
+     
+      console.log('dataset', dataset);
+     
+      xScale.domain(d3.range(dataset.length));
+      yScale.domain([0, d3.max(dataset, function(dataPoint) {
+          return dataPoint.amount; 
+        })]);
+      
+      var bars = svg.selectAll("rect").data(dataset, key);
+      
+      bars.enter()
+        .append("rect")
+        .attr("x", w)
+        .attr("y", function(d) {
+          return h - yScale(d.amount);
+        })
+        .attr("width", xScale.rangeBand())
+        .attr("height", function(d) {
+          return yScale(d.amount);  
+        })
+        .attr("fill", function(d) {
+          return "rgb(0, 0, " + (d.amount * 10) + ")";
+        })
+        .attr("data-id", function(d) {
+          return d._id;
+        });
+        
+      bars.transition()
+        .duration(500)
+        .attr("x", function(d, i) {
+          return xScale(i);
+        })
+        .attr("y", function(d) {
+          return h - yScale(d.amount);
+        })
+        .attr("width", xScale.rangeBand())
+        .attr("height", function(d) {
+          return yScale(d.amount);
+        })
+        .attr("fill", function(d) {
+          return "rgb(0, 0, " + (d.amount * 10) + ")";
+        });
+        
+      bars.exit()
+        .transition()
+        .duration()
+        .attr("x", -xScale.rangeBand())
+        .remove();
+        
+    });
+  };
 }
 
 if (Meteor.isServer) {
